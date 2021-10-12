ee9ca591c3cb823208e3df0264debb3670038f57
Alumno <jordis2311@gmail.com>
2021-10-12 11:31:39 -0300

    arreglados suicidios

--- asesino/asesino.py
+++ asesino/asesino.py
@@ -23,7 +23,7 @@ con una sola funcion?
 ¿Deberiamos haber separado las funciones en distintos archivos como haciamos en C?
 Me refiero a usar un archivo para la signatura, otro para la definicion, y otro para usar todo.
 
-¿Se puede modificar la forma en que un lenguaje esta hecho? 
+¿Se puede modificar la forma en que un lenguaje esta hecho?
 Me refiero a romper las reglas de sintaxis que son evaluadas, o definir otras nuevas.
 
 ¿Esta al tanto de que tranquilamente deberia haber googleado todas estas preguntas
@@ -44,7 +44,7 @@ Distancia == (String, String, Float)
 Donde:
 La primera componente representa el nombre de una region.
 La segunda componente representa el nombre de una region.
-La tercera componente representa la distancia entre ambas regiones. 
+La tercera componente representa la distancia entre ambas regiones.
 (¿Unidad? Kilometros, milimetros, codos)
 
 Para facilitar el acceso a las distancias entre regiones
@@ -263,19 +263,19 @@ def dividirPorRegion(lista_jugadores):
 # TOMA un string que representa el nombre del archivo de distancias.
 # DEVUELVE una lista de tuplas que indican la distancia
 def crearListaDistancias(archivo_distancias):
-	
+
 	lista_distancias = []
 	archivo = open(archivo_distancias, "r+")
 	for linea in archivo:
-		
+
 		# Separamos los datos
 		datos = linea.split(", ")
 
 		# Eliminamos el \n
 		datos[2] = datos[2][:-1]
-		
+
 		lista_distancias += [(datos[0],datos[1],float(datos[2]))]
-	
+
 	# Cerramos el archivo porque no lo necesitamos mas.
 	archivo.close()
 
@@ -287,24 +287,24 @@ def crearListaDistancias(archivo_distancias):
 # TOMA una lista de distancias
 # DEVUELVE un diccionario anidado con las distancias entre regiones.
 def crearDiccionarioDistancias(lista_distancias):
-	
+
 	lista_diccionarios = []
 	region = ""
 	# Empieza en -1 ya que la primera vez que se ejecute sera siempre nuevo y seteara a 0.
 	iterador = -1
-	
+
 	for distancia in lista_distancias:
-		
-		# Si la region no esta en la lista la agrega y crea su diccionario. 
+
+		# Si la region no esta en la lista la agrega y crea su diccionario.
 		if region != distancia[0]:
 			lista_diccionarios += [(distancia[0], dict([(distancia[1],distancia[2])]))]
 			region = distancia[0]
 			iterador += 1
 
-		# Si la region ya esta en la lista, actualiza su diccionario con el nuevo valor. 
+		# Si la region ya esta en la lista, actualiza su diccionario con el nuevo valor.
 		else:
 			lista_diccionarios[iterador][1].update([(distancia[1],distancia[2])])
-	
+
 	# Creamos un diccionario con los diccionarios de cada region.
 	return dict(lista_diccionarios)
 
@@ -312,11 +312,10 @@ def crearDiccionarioDistancias(lista_distancias):
 # juegoDelAsesino: Lista[Jugador] DiccionarioDistancias Int String -> List[Jugador]
 # TOMA una lista de jugadores, un diccionario de distancias, una maxima distancia y
 # el nombre del archivo resultante.
-# DEVUELVE una lista que representa los ganadores del juego. 
+# DEVUELVE una lista que representa los ganadores del juego.
 def juegoDelAsesino(Lista_de_jugadores,Diccionario_de_distancias,n,archivo):
 
 	final = True
-	iterador = 0
 	while (final):
 		Turno_parte_1 = emparejamientoPorRegion(Lista_de_jugadores)
 		lista_de_parejas = Turno_parte_1[0]
@@ -324,10 +323,10 @@ def juegoDelAsesino(Lista_de_jugadores,Diccionario_de_distancias,n,archivo):
 		lista_de_parejas += Turno_parte_2[0]
 		if lista_de_parejas == []:
 			final = False
-		lista_sobrevivientes = combate(lista_de_parejas,archivo)
-		lista_sobrevivientes += Turno_parte_2[1]
-		Lista_de_jugadores = dividirPorRegion(lista_sobrevivientes)
-		iterador += 1
+		else:
+			lista_sobrevivientes = combate(lista_de_parejas,archivo)
+			lista_sobrevivientes += Turno_parte_2[1]
+			Lista_de_jugadores = dividirPorRegion(lista_sobrevivientes)
 	return lista_sobrevivientes
 
 
@@ -337,14 +336,14 @@ def juegoDelAsesino(Lista_de_jugadores,Diccionario_de_distancias,n,archivo):
 def combate(lista_parejas, archivo_resultado):
     lista_ganadores = []
     for pareja in lista_parejas:
-        
+
         # Decidimos aleatoriamente quien gana y consecuentemente quien pierde.
         ganador = random.randrange(2)
         perdedor = 1 - ganador
         lista_ganadores += [pareja[ganador]]
 
-        archivo_resultado.write(pareja[ganador][0] + " elimino a " + pareja[perdedor][0] + "\n")
-    return lista_ganadores        
+        archivo_resultado.write(pareja[ganador][0] + " " + str(pareja[ganador][1]) + " " + " elimino a " + pareja[perdedor][0] + " " + str(pareja[perdedor][1]) + " " + "\n")
+    return lista_ganadores
 
 
 # emparejamientoPorRegion: List[List[Jugadores]] -> (List[Pareja], List[Jugador])
@@ -374,9 +373,9 @@ def emparejamientoSobrantes(lista_jugadores_sobrantes, diccionario_distancias, n
 
 	lista_parejas = []
 	sobrantes_de_las_sobras = []
-	cantidad_jugadores = len(lista_jugadores_sobrantes)  
+	cantidad_jugadores = len(lista_jugadores_sobrantes)
 	while cantidad_jugadores > 1:
-		
+
 		# Elegimos uno y lo sacamos
 		eleccion_1 = lista_jugadores_sobrantes[random.randrange(cantidad_jugadores)]
 		cantidad_jugadores -= 1
@@ -388,10 +387,13 @@ def emparejamientoSobrantes(lista_jugadores_sobrantes, diccionario_distancias, n
 		# Elegimos el candidato con la menor distancia.
 		eleccion_2 = jugadorMasCercano(lista_jugadores_sobrantes, lista_cercanias, n)
 
+
 		# Si el candidato es valido, lo empareja.
-		if eleccion_2 != 0:
+		if eleccion_2 != ("",0,""):
 			lista_parejas += [(eleccion_1, eleccion_2)]
-		
+			cantidad_jugadores -= 1
+			lista_jugadores_sobrantes.remove(eleccion_2)
+
 		# Sino, lo descarta.
 		else:
 			sobrantes_de_las_sobras += [eleccion_1]
@@ -407,7 +409,7 @@ def preferenciaDistancias(objetivo, lista_jugadores, diccionario_distancias):
 	lista_distancias = []
 
 	for jugador in lista_jugadores:
-		
+
 		if objetivo[2] in diccionario_distancias:
 			if jugador[2] in diccionario_distancias[objetivo[2]]:
 				lista_distancias += [diccionario_distancias[objetivo[2]][jugador[2]]]
@@ -416,7 +418,7 @@ def preferenciaDistancias(objetivo, lista_jugadores, diccionario_distancias):
 		else:
 			lista_distancias += [diccionario_distancias[jugador[2]][objetivo[2]]]
 
-	return lista_distancias		
+	return lista_distancias
 
 
 # jugadorMasCercano: List[Jugador] List[Int] Int -> Int || Jugador
@@ -424,10 +426,10 @@ def preferenciaDistancias(objetivo, lista_jugadores, diccionario_distancias):
 # y una distancia maxima posible.
 # EVALUA si la menor distancia de la lista es menor o igual que la maxima distancia posible.
 # Si esto ocurre, devuelve el jugador correspondiente a dicha distancia.
-# Si no, devuelve un entero (0), representando que no tiene candidato.
+# Si no, devuelve un jugador nulo
 def jugadorMasCercano(lista_jugadores, lista_distancias, n):
-	
-	candidato = 0
+
+	candidato = ("",0,"")
 	menor_distancia = min(lista_distancias)
 	indice_menor_distancia = lista_distancias.index(menor_distancia)
 	jugador_mas_cercano = lista_jugadores[indice_menor_distancia]
@@ -452,11 +454,11 @@ def separarListaEdad(lista):
 
 
 # crearListaJugadores: String -> Lista[Jugadores]
-# TOMA un string que representa el nombre del archivo 
+# TOMA un string que representa el nombre del archivo
 # que contiene la informacion sobre los jugadores.
 # DEVUEVLE una lista de jugadores.
 def crearListaJugadores(archivo_jugadores):
-    
+
 	# Abrimos el archivo.
     archivo = open(archivo_jugadores,"r+")
     # Creamos la lista que vamos a devolver.
@@ -464,22 +466,22 @@ def crearListaJugadores(archivo_jugadores):
 
     # Recorremos el archivo por lineas.
     for linea in archivo:
-        
+
         # Los datos del jugador estan separados por comas.
         jugador = linea.split(",")
-        
+
         # Guardamos el nombre.
         nombre = jugador[0]
-        
+
         # Guardamos la edad.
         edad = int(jugador[1])
-        
+
         # Guardamos la ciudad sin el ultimo caracter que es una nueva linea '\n'
         ciudad = jugador[2][:-1]
-        
+
         # Creamos una tupla que contiene los datos de cada jugador y la almacenamos en la lista.
         lista_jugadores += [(nombre,edad,ciudad)]
-    
+
     # Cerramos el archivo.
     archivo.close()
 
